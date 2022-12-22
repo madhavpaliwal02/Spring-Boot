f871afe511ce1c616baf4e4a56a1ac2a169d3f24
Madhav Paliwal <madhavpaliwal02@gmail.com>
2022-12-22 18:13:01 +0530

    Contact-service is configured with eureka

--- demo-3/demo-3/src/main/java/com/crud/service/ContactServiceImpl.java
+++ demo-3/demo-3/src/main/java/com/crud/service/ContactServiceImpl.java
@@ -12,8 +12,10 @@ public class ContactServiceImpl implements ContactService {
 
 	// fake contacts
 
-	List<Contact> list2 = List.of(new Contact(1, "Agar", "MP", 27), new Contact(2, "Indore", "MP", 39),
+	List<Contact> list2 = java.util.List.of(new Contact(1, "Agar", "MP", 27), new Contact(2, "Indore", "MP", 39),
 			new Contact(3, "Agar", "MP", 27), new Contact(4, "Barwah", "MP", 8), new Contact(5, "Indore", "MP", 11));
+
+
 
 	@Override
 	public List<Contact> getContact(int userId) {
