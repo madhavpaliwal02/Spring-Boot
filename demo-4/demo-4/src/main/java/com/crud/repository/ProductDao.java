e36ebb0521b7360f5798487d93236a0dd251a29b
Madhav Paliwal <madhavpaliwal02@gmail.com>
2022-12-22 18:11:33 +0530

    Created & configured Eureka server

--- demo-4/demo-4/src/main/java/com/crud/repository/ProductDao.java
+++
@@ -1,9 +0,0 @@
-package com.crud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crud.model.Product;
-
-public interface ProductDao extends JpaRepository<Product, Integer> {
-
-}
